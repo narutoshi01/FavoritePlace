018efe717daf5da343ff54387645e4693d42f4f3
sakamichi-jewel <taigayyy@gmail.com>
2020-05-20 20:19:22 +0900

    create UI for EditPlaceActivity

---
+++ app/src/main/java/com/narutoshi/favoriteplace/EditPlaceActivity.kt
@@ -0,0 +1,12 @@
+package com.narutoshi.favoriteplace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditPlaceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_place)
+    }
+}
