8c142c9ccf091009ba7a443535ecc37e9d1ca9b6
sakamichi-jewel <taigayyy@gmail.com>
2020-05-20 20:52:10 +0900

    create Intent from MainActivity to EditPlaceActivity

---
+++ app/src/main/java/com/narutoshi/favoriteplace/Constants.kt
@@ -0,0 +1,5 @@
+package com.narutoshi.favoriteplace
+
+object RequestCode {
+    const val EDIT_PLACE_ACTIVITY_REQUEST_CODE = 1
+}
--- app/src/main/java/com/narutoshi/favoriteplace/MainActivity.kt
+++ app/src/main/java/com/narutoshi/favoriteplace/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.narutoshi.favoriteplace
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fab.setOnClickListener {
+            val intent = Intent(this, EditPlaceActivity::class.java)
+            startActivityForResult(intent, RequestCode.EDIT_PLACE_ACTIVITY_REQUEST_CODE)
+        }
+
     }
 }
