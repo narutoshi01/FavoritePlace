5de9b89858d5a309617891fcfad2e503bb989c9f
sakamichi-jewel <taigayyy@gmail.com>
2020-05-21 08:30:27 +0900

    create UI for PlaceDetailActivity

---
+++ app/src/main/java/com/narutoshi/favoriteplace/activities/PlaceDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.narutoshi.favoriteplace.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.narutoshi.favoriteplace.R
+
+class PlaceDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_place_detail)
+    }
+}
