dff67454ef0db1e9524cc570e977a4462a3c4314
sakamichi-jewel <taigayyy@gmail.com>
2020-05-21 09:09:34 +0900

    PlaceDetailActivity: get info from MainActivity and show them on screen

--- app/src/main/java/com/narutoshi/favoriteplace/activities/PlaceDetailActivity.kt
+++ app/src/main/java/com/narutoshi/favoriteplace/activities/PlaceDetailActivity.kt
@@ -2,12 +2,37 @@ package com.narutoshi.favoriteplace.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.narutoshi.favoriteplace.IntentKey
 import com.narutoshi.favoriteplace.R
+import kotlinx.android.synthetic.main.activity_place_detail.*
 
 class PlaceDetailActivity : AppCompatActivity() {
+
+    private var title: String? = null
+    private var description: String? = null
+    private var date: String? = null
+    private var imageURI: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_place_detail)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+        title = intent.getStringExtra(IntentKey.TITLE)
+        description = intent.getStringExtra(IntentKey.DESCRIPTION)
+        date = intent.getStringExtra(IntentKey.DATE)
+        imageURI = intent.getStringExtra(IntentKey.IMAGE_URI)
+
+        tv_title.text = title
+        tv_description.text = description
+        tv_date.text = date
+        // TODO image view に取得したURIを使って画像をセットする
     }
+
+    
 }
