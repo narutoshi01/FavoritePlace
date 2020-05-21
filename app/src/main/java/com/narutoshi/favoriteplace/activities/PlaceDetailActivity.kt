0bd4713ae0af3ab4f6e92a3b9050691c54aa8a48
sakamichi-jewel <taigayyy@gmail.com>
2020-05-21 09:15:52 +0900

    PlaceDetailActivity: set up toolbar option menu

--- app/src/main/java/com/narutoshi/favoriteplace/activities/PlaceDetailActivity.kt
+++ app/src/main/java/com/narutoshi/favoriteplace/activities/PlaceDetailActivity.kt
@@ -2,6 +2,9 @@ package com.narutoshi.favoriteplace.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.narutoshi.favoriteplace.IntentKey
 import com.narutoshi.favoriteplace.R
 import kotlinx.android.synthetic.main.activity_place_detail.*
@@ -34,5 +37,32 @@ class PlaceDetailActivity : AppCompatActivity() {
         // TODO image view に取得したURIを使って画像をセットする
     }
 
-    
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+
+        menuInflater.inflate(R.menu.menu_main, menu)
+        menu?.apply {
+            findItem(R.id.action_delete).isVisible = true
+            findItem(R.id.action_edit).isVisible = true
+            findItem(R.id.action_register).isVisible = false
+        }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_delete -> {
+                // TODO レコード削除。リスト一覧へ戻る
+                Toast.makeText(this, "action delete", Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.action_edit -> {
+                // TODO EditActivityへ。modeはEditで。インテントで情報渡す
+                Toast.makeText(this, "action edit", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
 }
